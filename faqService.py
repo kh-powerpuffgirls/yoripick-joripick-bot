fae29a8fc21d0511d23ca9f2cb188fb18cc792c2
530hyelim <530hyelim@gmail.com>
2025-10-06 01:31:23 +0900

    Update faqService.py

--- faqService.py
+++ faqService.py
@@ -144,7 +144,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://front.ypjp.store", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
